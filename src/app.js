32649a586dcf404c796464294822329c6a8b6ead
0xboz <0xboz@hacari.com>
2019-04-22 16:38:31 -0400

    Added babel-polyfill

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
-let test = () => console.log(123);
+import 'babel-polyfill';
+import {person, sayHello} from './lib';
 
+console.log(person.name);
+
+console.log(sayHello(person.name));
+
+
+async function getPosts() {
+    const response = await fetch(
+        "https://jsonplaceholder.ir/posts"
+    );
+    const data = await response.json();
+    return data;
+}
+
+getPosts().then(posts => console.log(posts));
